ef171966cdb1f753038cc04b3e8558ffa4038bdc
wikilike7 <wikilike7@gmail.com>
2017-02-10 11:58:04 +0800

    modify to right code

--- IE-Proxy-Switch-Tool.bat
+++ IE-Proxy-Switch-Tool.bat
@@ -1,11 +1,12 @@
 @echo off
-title 代理切换工具
-color 1 7
-echo 1 == 香港代理
-echo 2 == 大陆代理
-echo 3 == 无代理
+title IE Proxy Switch Tool
+rem COLOR 97
+echo 1. HK Proxy 
+echo 2. China Proxy
+echo 3. No Proxy
 
-set /p user_choose=请选择对应的数字并按回车： 
+rem Get user choose and jump to corresponding code
+set /p user_choose=Enter the corresponding number and press Enter to complete switch:  
 if "%user_choose%"=="1" goto hk_proxy
 if "%user_choose%"=="2" goto china_proxy
 if "%user_choose%"=="3" goto no_proxy
@@ -14,17 +15,14 @@ if "%user_choose%"=="3" goto no_proxy
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "172.29.225.90:8080" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "172.*;rechwebportal;<local>" /f
-pause
 exit
 
 :china_proxy
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 1 /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyServer /t REG_SZ /d "172.28.15.24:8080" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyOverride /t REG_SZ /d "172.*;rechwebportal;<local>" /f
-pause
 exit
 
 :no_proxy
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Internet Settings" /v ProxyEnable /t REG_DWORD /d 0 /f
-pause
 exit
